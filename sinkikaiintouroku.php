c05a96d7cf7bf09c9214f4cfb4b93c91f159e2ba
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-10-10 10:43:01 +0900

    新規会員登録php

---
+++ sinkikaiintouroku.php
@@ -0,0 +1,11 @@
+<?php
+const SERVER = 'mysql326.phy.lolipop.lan';
+const DBNAME = 'LAA1607632-pocketroom';
+const USER = 'LAA1607632';
+const PASS = 'pocketRoom';
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<?php
+$pdo = new PDO($connect,USER,PASS);
+
+?>
