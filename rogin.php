6a5a9aefe319dcf97fc78b0d5b11bf01fdb3711a
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-10-15 11:54:59 +0900

    php updata

---
+++ rogin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+const SERVER = 'mysql326.phy.lolipop.lan';
+const DBNAME = 'LAA1607632-pocketroom';
+const USER = 'LAA1607632';
+const PASS = 'pocketRoom';
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+
+$pdo= new PDO($connect,USER,PASS);
+$sql= "select * from customer where login=? and password=?";
+$stmt= $pdo->prepare($sql);
+$stmt->execute([$_POST['login'],$_POST['password']]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user && $password) {
+        // ログイン成功
+        $_SESSION['user'] = $user['customer_name'];
+        echo "<p>ログイン成功！ようこそ、" . htmlspecialchars($user['customer_name']) . " さん。</p>";
+        echo "<a href='mypage.php'>マイページへ進む</a>";
+    } else {
+        // ログイン失敗
+        echo "<p>ユーザーネームまたはパスワードが間違っています。</p>";
+        echo "<a href='login.html'>戻る</a>";
+    }
+?>
+?>
--- sinkikaiintouroku.php
+++ sinkikaiintouroku.php
@@ -1,4 +1,15 @@
 <?php
+$name       = $_POST['ustomer_name'];
+$kana       = $_POST['kana'];
+$birth      = $_POST['birth'];
+$prefecture = $_POST['prefecture'];
+$city       = $_POST['city'];
+$address    = $_POST['address'];
+$building   = $_POST['building'];
+$phone      = $_POST['phone'];
+$email      = $_POST['email'];
+$password   = $_POST['password'];
+
 const SERVER = 'mysql326.phy.lolipop.lan';
 const DBNAME = 'LAA1607632-pocketroom';
 const USER = 'LAA1607632';
@@ -7,5 +18,6 @@ const PASS = 'pocketRoom';
 ?>
 <?php
 $pdo = new PDO($connect,USER,PASS);
-$sql= "insert into customer(customer_id,customer_name,address,login,password) values()"
+$sql= "insert into customer(customer_id,customer_name,address,login,password) values(?,?,?,?,?,?,?,?,?,?)";
+$pdo->prepare($sql);
 ?>
