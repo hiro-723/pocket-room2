ab4d266e122a97538404c868adadd65fb621fa36
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-10-20 11:39:19 +0900

    Update rogin.php

--- rogin.php
+++ rogin.php
@@ -7,13 +7,12 @@ const PASS = 'pocketRoom';
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 
 $pdo= new PDO($connect,USER,PASS);
-$sql= "select * from customer where login=? and password=?";
+$sql= "select * from customer where customer_name=? and password=?";
 $stmt= $pdo->prepare($sql);
-$stmt->execute([$_POST['login'],$_POST['password']]);
+$stmt->execute([$_POST['username'],$_POST['password']]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
-$password = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($user && $password) {
+if ($user) {
         // ログイン成功
         $_SESSION['user'] = $user['customer_name'];
         header("Location: home.php");
