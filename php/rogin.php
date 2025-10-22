a112c765a1cfa81f70905ee6c3601da0caead12d
user-1234-dayo <2401183@s.asojuku.ac.jp>
2025-10-22 11:47:04 +0900

    updata

--- php/rogin.php
+++ php/rogin.php
@@ -1,27 +1,24 @@
 <?php
 session_start();
-const SERVER = 'mysql326.phy.lolipop.lan';
-const DBNAME = 'LAA1607632-pocketroom';
-const USER = 'LAA1607632';
-const PASS = 'pocketRoom';
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+require_once 'db_connect.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$pdo= new PDO($connect,USER,PASS);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql= "select * from customer where customer_name=? and password=?";
-$stmt= $pdo->prepare($sql);
-$stmt->execute([$_POST['username'],$_POST['password']]);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+    // ユーザー名で検索
+    $sql = "SELECT * FROM customer WHERE customer_name = ? AND password = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$username, $password]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($user) {
+    if ($user) {
         // ログイン成功
-        $_SESSION['user'] = $user['customer_name'];
-        header("Location: home.php");
+        $_SESSION['customer_id'] = $user['customer_id'];
+         $_SESSION['username'] = $user['customer_name'];
+        header("Location: home.html"); // ホーム画面へ
         exit;
     } else {
-        // ログイン失敗
-        echo "<p>ユーザーネームまたはパスワードが間違っています。</p>";
-        echo "<a href='rogin.html'>戻る</a>";
-        exit;
+        echo "ユーザー名またはパスワードが間違っています。";
     }
+}
 ?>
